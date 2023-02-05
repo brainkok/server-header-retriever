4486fd782e2a9d781f683aa750196972c2c72c37
brainkok <92050069+brainkok@users.noreply.github.com>
2023-02-05 22:38:17 +0100

    Added X-Powered-By header and version mode

--- server-header-retriever.py
+++ server-header-retriever.py
@@ -1,7 +1,8 @@
+#!/usr/bin/python3
 import http.client
 import ssl
 
-def retrieve_server_header(filename):
+def retrieve_headers(filename, show_version_only=True):
     with open(filename) as f:
         websites = [line.strip() for line in f.readlines()]
 
@@ -12,16 +13,20 @@ def retrieve_server_header(filename):
             conn.request("GET", "/")
             res = conn.getresponse()
             server = res.getheader('Server')
-            if server and any(char.isdigit() for char in server):
+            x_powered_by = res.getheader('X-Powered-By')
+            if show_version_only or (server and any(char.isdigit() for char in server)):
                 print(f"Server header of {website}: {server}")
+            if show_version_only or (x_powered_by and any(char.isdigit() for char in x_powered_by)):
+                print(f"X-Powered-By header of {website}: {x_powered_by}")
         except Exception as e:
             pass
 
 if __name__ == "__main__":
     import sys
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <filename>.txt")
+    if len(sys.argv) not in (2, 3):
+        print("Usage: python script.py <filename>.txt [--version-only]")
         sys.exit()
 
     filename = sys.argv[1]
-    retrieve_server_header(filename)
+    show_version_only = len(sys.argv) == 2 or sys.argv[2] != "--version-only"
+    retrieve_headers(filename, show_version_only)
