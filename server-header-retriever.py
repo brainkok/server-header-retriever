21073034163ff9a43ba7553d376156e6689b9c8d
brainkok <92050069+brainkok@users.noreply.github.com>
2023-02-05 21:10:31 +0100

    Create server-header-retriever.py

---
+++ server-header-retriever.py
@@ -0,0 +1,23 @@
+import http.client
+import ssl
+import sys
+
+def get_server_header(file):
+    with open(file) as f:
+        websites = [line.strip() for line in f.readlines()]
+
+    for website in websites:
+        website = website.replace("http://", "").replace("https://", "")
+        context = ssl.create_default_context()
+        context.check_hostname = False
+        context.verify_mode = ssl.CERT_NONE
+        try:
+            conn = http.client.HTTPSConnection(website, context=context)
+            conn.request("GET", "/")
+            res = conn.getresponse()
+            print(f"Server header van {website}: {res.getheader('Server')}")
+        except:
+            print(f"Kan geen verbinding maken met {website}")
+
+file = sys.argv[1]
+get_server_header(file)
